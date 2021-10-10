11d2bc476c830a6ba7bb61fece71583074f568e4
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-10-10 15:15:46 +0000

    ネスペ

--- src/lib.rs
+++ src/lib.rs
@@ -27,9 +27,9 @@ pub async fn run() -> Result<Output, Box<dyn std::error::Error>> {
 fn generate_message() -> String {
     let tz = chrono_tz::Asia::Tokyo;
     let now = Utc::now().with_timezone(&tz);
-    let shikenbi = Utc.ymd(2021, 10, 10).with_timezone(&tz);
+    let shikenbi = Utc.ymd(2022, 4, 17).with_timezone(&tz);
     format!(
-        "エンベデッドシステムスペシャリスト試験まで{}日です。勉強してください。",
+        "ネットワークスペシャリスト試験まで{}日です。勉強してください。",
         (shikenbi - now.date()).num_days()
     )
 }
@@ -66,7 +66,7 @@ fn generate_header(token: &str) -> HeaderMap {
 fn aa() {
     let tz = chrono_tz::Asia::Tokyo;
     let datetime = Utc::now().with_timezone(&tz);
-    let aaa = Utc.ymd(2021, 10, 10).with_timezone(&tz);
+    let aaa = Utc.ymd(2021, 6, 132).with_timezone(&tz);
     dbg!(datetime);
     dbg!(aaa);
     dbg!((aaa - datetime.date()).num_days());
